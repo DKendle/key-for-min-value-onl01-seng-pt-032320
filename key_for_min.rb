9c785538ec42a12ea98d7b4546934d4059c376b1
Dina Kendle <dina.kendle@gmail.com>
2020-04-04 17:43:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,10 @@
 
 def key_for_min_value(name_hash)
 
-  name_hash.each do |name, value|
     smallest_key = nil
     smallest_value = nil
-    
+    name_hash.each do |name, value|
+      if smallest_value == nil || smallest_value < 
     
     end
 end
